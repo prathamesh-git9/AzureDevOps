94f430dac4d11c98873c2551b61a487edb9c6bf5
Mitpatel11 <87959696+Mitpatel11@users.noreply.github.com>
2024-11-11 00:43:18 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -41,54 +41,6 @@ data "aws_key_pair" "existing_key" {
 }
 
 
-terraform {
-  required_providers {
-    docker = {
-      source  = "kreuzwerker/docker"
-      version = "~> 2.0"
-    }
-  }
-}
-
-provider "docker" {}
-
-# Pull an Image from Docker Hub
-resource "docker_image" "nginx" {
-  name = "nginx:latest"
-  keep_locally = false  # Set to true if you want to keep it after the container is deleted
-}
-
-# Define a Container that Uses the Pulled Image
-resource "docker_container" "nginx_container" {
-  image = docker_image.nginx.latest
-  name  = "my-nginx-container"
-
-  ports {
-    internal = 80
-    external = 8080
-  }
-}
-
-# Output the container's IP address
-output "nginx_container_ip" {
-  value = docker_container.nginx_container.ip_address
-}
-
-# EC2 Instance using the key pair and security group
-resource "aws_instance" "vm" {
-  ami           = "ami-03ca36368dbc9cfa1"  # Replace with your preferred AMI ID
-  instance_type = "t2.micro"
-  key_name      = data.aws_key_pair.existing_key.key_name   # Reference the existing key pair
-
-  # Associate the EC2 instance with the security group
-  vpc_security_group_ids = [aws_security_group.vm_sg.id]
-
-  tags = {
-    Name = "TerraformVM"
-  }
-}
-
-
 output "vm_ip" {
   value = aws_instance.vm.public_ip
 }
